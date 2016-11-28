cc587304221c07eb8830f76435fa3bcfaa9d4869
Daniel Casner <daniel@danielcasner.org>
2016-11-28 02:46:31 +0000

    Working door controller, now also with built in test script.

--- door.py
+++ door.py
@@ -6,7 +6,13 @@ Logic for controlling the chicken coop door.
 import pigpio
 import logging
 import json
-from drivers.DRV8871 import Motor
+from DRV8871 import Motor
+
+COOP_OPEN_SW   = 17
+COOP_CLOSED_SW =  4
+COOP_MOT_IN1   = 0x10e
+COOP_MOT_IN2   = 0x10f
+
 
 class Door:
     """Object for door control"""
@@ -14,10 +20,10 @@ class Door:
     DOOR_OPEN_TOKEN   = json.dumps("OPEN")
     DOOR_CLOSED_TOKEN = json.dumps("CLOSED")
     DOOR_AJAR_TOKEN   = json.dumps('AJAR')
-    OPEN_TIMEOUT_MS   = 10000
-    CLOSE_TIMEOUT_MS  = 10000
-    OPEN_SPEED        = -5.0
-    CLOSE_SPEED       =  0.4
+    OPEN_TIMEOUT_MS   = 20000
+    CLOSE_TIMEOUT_MS  = 20000
+    OPEN_SPEED        = -1.0
+    CLOSE_SPEED       =  0.5
     LATCH_SPEED       =  1.0
     LATCH_DURATION    =  1.0
     GLITCH_FILTER_µs  = 3000
@@ -29,11 +35,11 @@ class Door:
         self.motor = Motor(pi, in1, in2)
         pi.set_mode(closed_sw, pigpio.INPUT)
         pi.set_pull_up_down(closed_sw, pigpio.PUD_UP)
-        pi.set_glitch_filter(closed_sw, GLITCH_FILTER_µs)
+        pi.set_glitch_filter(closed_sw, self.GLITCH_FILTER_µs)
         pi.set_mode(open_sw, pigpio.INPUT)
         pi.set_pull_up_down(open_sw, pigpio.PUD_UP)
-        pi.set_glitch_filter(open_sw, GLITCH_FILTER_µs)
-        self.closSw = closed_sw
+        pi.set_glitch_filter(open_sw, self.GLITCH_FILTER_µs)
+        self.closed_sw = closed_sw
         self.open_sw = open_sw 
         self.logger = logging.getLogger(__name__)
         self.client = client
@@ -46,8 +52,8 @@ class Door:
     
     def stop(self):
         self.motor.stop()
-        self.pi.set_watchdoog(self.open_sw,   0)
-        self.pi.set_watchdoog(self.closed_sw, 0)
+        self.pi.set_watchdog(self.open_sw,   0)
+        self.pi.set_watchdog(self.closed_sw, 0)
         self.check_status_and_publsh()
     
     def publish_open(self):
@@ -59,7 +65,7 @@ class Door:
     def publish_ajar(self):
         self.client.publish(self.door_status_topic, self.DOOR_AJAR_TOKEN, qos=1, retain=True)
     
-    def check_status_and_publsh():
+    def check_status_and_publsh(self):
         if self.pi.read(self.open_sw) == 0:
             self.publish_open()
         elif self.pi.read(self.closed_sw) == 0:
@@ -69,7 +75,7 @@ class Door:
     
     def _open_cb(self, gpio, level, tick):
         "Callback when open_sw is triggered"
-        if level == 0:
+        if level == 1:
             # Door finished opening
             self.motor.stop()
             self.pi.set_watchdog(self.open_sw, 0)
@@ -83,10 +89,10 @@ class Door:
             self.logger.error("Unexpected door open switch status: {}".format(level))
     
     def _close_cb(self, gpio, level, tick):
-        "Callback when close_sw is triggered"
-        if level == 0:
+        "Callback when closed_sw is triggered"
+        if level == 1:
             # Door finished closing
-            self.pi.set_watchdog(self.close_sw, 0)
+            self.pi.set_watchdog(self.closed_sw, 0)
             self.motor.drive(self.LATCH_SPEED)
             time.sleep(self.LATCH_DURATION)
             self.motor.stop()
@@ -103,25 +109,47 @@ class Door:
         "Trigger the door to open, optionally set a multiple of normal speed."
         if not self.enabled:
             return
-        elif self.pi.read(self.open_sw) == 0:
+        elif self.pi.read(self.open_sw) == 1:
             self.logger.debug("Door already open")
         else:
-            self.pi.callback(self.open_sw, pigpio.FALLING_EDGE, self._open_cb)
+            self.pi.callback(self.open_sw, pigpio.RISING_EDGE, self._open_cb)
             self.pi.set_watchdog(self.open_sw, self.OPEN_TIMEOUT_MS)
             self.motor.drive(self.OPEN_SPEED * speed)
-            self.logger.debug("Dooor opening")
+            self.logger.debug("Door opening")
     
     def close(self, speed=1.0):
         "Trigger the door to close, optionally set a multiple of normal speed"
         if not self.enabled:
             return
-        elif self.pi.read(self.closed_sw) == 0:
+        elif self.pi.read(self.closed_sw) == 1:
             self.logger.debug("Door already closed")
         else:
-            self.pi.callback(self.closed_sw, pigpio.FALLING_EDGE, self._close_cb)
+            self.pi.callback(self.closed_sw, pigpio.RISING_EDGE, self._close_cb)
             self.pi.set_watchdog(self.closed_sw, self.CLOSE_TIMEOUT_MS)
             self.motor.drive(self.CLOSE_SPEED * speed)
             self.logger.debug("Door closing")
 
     def enable(self, enabled):
         self.enabled = enabled
+
+        
+if __name__ == '__main__':
+    import sys
+    import time
+    import PCA9685_pigpio
+    class DummyClient:
+        def publish(self, *args, **kwargs):
+            pass
+
+    pi = PCA9685_pigpio.PCA9685Pi()
+    door = Door(pi, COOP_MOT_IN1, COOP_MOT_IN2, COOP_CLOSED_SW, COOP_OPEN_SW, DummyClient(), "DUMMY_DOOR")
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "open":
+            door.open()
+            time.sleep(door.OPEN_TIMEOUT_MS/1000.0)
+        elif sys.argv[1] == "close":
+            door.close()
+            time.sleep(door.CLOSE_TIMEOUT_MS/1000.0)
+
+    door.stop()

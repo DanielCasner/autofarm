6685db7fedcd485469044869fe38497ff9787f8b
Daniel Casner <daniel@danielcasner.org>
2017-10-01 11:06:54 -0700

    Make camera scripts wait for device to exist so systemd doesn't burn 200% CPU when the camera is missing.

--- camera-scripts/brooder.sh
+++ camera-scripts/brooder.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env sh
-cvlc --no-audio v4l2:///dev/video0 --v4l2-width 1280 --v4l2-height 720 --v4l2-chroma MJPG --v4l2-hflip 1 --v4l2-vflip 1 --sout '#standard{access=http{mime=multipart/x-mixed-replace;boundary=--7b3cc56e5f51db803f790dad720ed50a},mux=mpjpeg,dst=:8556/brooder}' -I dummy
+CAM=/dev/video0
+while ! test -e $CAM; do sleep 13; done
+cvlc --no-audio v4l2://$CAM --v4l2-width 1280 --v4l2-height 720 --v4l2-chroma MJPG --v4l2-hflip 1 --v4l2-vflip 1 --sout '#standard{access=http{mime=multipart/x-mixed-replace;boundary=--7b3cc56e5f51db803f790dad720ed50a},mux=mpjpeg,dst=:8556/brooder}' -I dummy
--- camera-scripts/run.sh
+++ camera-scripts/run.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env sh
-cvlc --no-audio v4l2:///dev/video1 --v4l2-width 1280 --v4l2-height 720 --v4l2-chroma MJPG --v4l2-hflip 1 --v4l2-vflip 1 --sout '#standard{access=http{mime=multipart/x-mixed-replace;boundary=--7b3cc56e5f51db803f790dad720ed50a},mux=mpjpeg,dst=:8555/run}' -I dummy
+CAM=/dev/video1
+while ! test -e $CAM; do sleep 13; done
+cvlc --no-audio v4l2://$CAM --v4l2-width 1280 --v4l2-height 720 --v4l2-chroma MJPG --v4l2-hflip 1 --v4l2-vflip 1 --sout '#standard{access=http{mime=multipart/x-mixed-replace;boundary=--7b3cc56e5f51db803f790dad720ed50a},mux=mpjpeg,dst=:8555/run}' -I dummy
